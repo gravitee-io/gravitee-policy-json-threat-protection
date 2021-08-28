c49ddfcbae41fc2e54a8d6b0f97f458f749d48a6
Jeoffrey Haeyaert <jeoffrey.haeyaert@gmail.com>
2021-08-28 12:30:00 +0200

    fix(array-size): properly check array size
    Closes gravitee-io/issues#6050

--- src/test/java/io/gravitee/policy/threatprotection/json/JsonThreatProtectionPolicyTest.java
+++ src/test/java/io/gravitee/policy/threatprotection/json/JsonThreatProtectionPolicyTest.java
@@ -26,10 +26,13 @@ import io.gravitee.policy.api.PolicyResult;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static io.gravitee.policy.threatprotection.json.JsonThreatProtectionPolicy.*;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
@@ -48,6 +51,80 @@ public class JsonThreatProtectionPolicyTest {
 
     @Mock
     private PolicyChain policyChain;
+
+    @Captor
+    private ArgumentCaptor<PolicyResult> resultCaptor;
+
+    private static final String JSON = "{\n" +
+            "    \"travel\": {\n" +
+            "        \"type\": \"TOURISM\",\n" +
+            "        \"language\": \"FR\",\n" +
+            "        \"isPolicyHolderTravelling\": true,\n" +
+            "        \"start\": \"2019-06-03\",\n" +
+            "        \"end\": \"2019-06-05\"\n" +
+            "    },\n" +
+            "    \"policyHolder\": {\n" +
+            "        \"civility\": \"xxxxxx\",\n" +
+            "        \"firstname\": \"xxxxxx\",\n" +
+            "        \"lastname\": \"xxxxxx\",\n" +
+            "        \"maidenname\": \"xxxxxx\",\n" +
+            "        \"id\": \"45465\",\n" +
+            "        \"phone\": \"0606060607\",\n" +
+            "        \"professionalPhone\": \"0101010101\",\n" +
+            "        \"email\": \"xxxxxxxxxxx\",\n" +
+            "        \"address\": {\n" +
+            "            \"street\": \"my street\",\n" +
+            "            \"zipcode\": \"xxxxxx\",\n" +
+            "            \"city\": \"xxxxxx\",\n" +
+            "            \"countryCode\": \"xxxxxx\"\n" +
+            "        }\n" +
+            "    },\n" +
+            "    \"beneficiaries\": [\n" +
+            "        {\n" +
+            "            \"civility\": \"xxxx\",\n" +
+            "            \"firstname\": \"xxxxx\",\n" +
+            "            \"lastname\": \"xxxxx\",\n" +
+            "            \"maidenname\": \"xxxxxxx\",\n" +
+            "            \"relationtopolicyholder\": \"xxxxxxx\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"civility\": \"xxxx\",\n" +
+            "            \"firstname\": \"xxxxx\",\n" +
+            "            \"lastname\": \"xxxxx\",\n" +
+            "            \"maidenname\": \"xxxxxxx\",\n" +
+            "            \"relationtopolicyholder\": \"xxxxxxx\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"civility\": \"xxxx\",\n" +
+            "            \"firstname\": \"xxxxx\",\n" +
+            "            \"lastname\": \"xxxxx\",\n" +
+            "            \"maidenname\": \"xxxxxxx\",\n" +
+            "            \"relationtopolicyholder\": \"xxxxxxx\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"civility\": \"xxxx\",\n" +
+            "            \"firstname\": \"xxxxx\",\n" +
+            "            \"lastname\": \"xxxxx\",\n" +
+            "            \"maidenname\": \"xxxxxxx\",\n" +
+            "            \"relationtopolicyholder\": \"xxxxxxx\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"civility\": \"xxxx\",\n" +
+            "            \"firstname\": \"xxxxx\",\n" +
+            "            \"lastname\": \"xxxxx\",\n" +
+            "            \"maidenname\": \"xxxxxxx\",\n" +
+            "            \"relationtopolicyholder\": \"xxxxxxx\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"civility\": \"xxxx\",\n" +
+            "            \"firstname\": \"xxxxx\",\n" +
+            "            \"lastname\": \"xxxxx\",\n" +
+            "            \"maidenname\": \"xxxxxxx\",\n" +
+            "            \"relationtopolicyholder\": \"xxxxxxx\"\n" +
+            "        }\n" +
+            "    ]\n" +
+            "}";
+
 
     JsonThreatProtectionPolicyConfiguration configuration;
 
@@ -103,7 +180,8 @@ public class JsonThreatProtectionPolicyTest {
         readWriteStream.write(Buffer.buffer("Invalid"));
         readWriteStream.end();
 
-        verify(policyChain, times(1)).streamFailWith(any(PolicyResult.class));
+        verify(policyChain, times(1)).streamFailWith(resultCaptor.capture());
+        assertEquals(JSON_THREAT_DETECTED_KEY, resultCaptor.getValue().key());
     }
 
     @Test
@@ -117,7 +195,8 @@ public class JsonThreatProtectionPolicyTest {
         readWriteStream.write(Buffer.buffer("{ \"valid\": true, \"array\": [ 1, 2, 3 ], \"container\": { \"a\": \"123456789\" } }"));
         readWriteStream.end();
 
-        verify(policyChain, times(1)).streamFailWith(any(PolicyResult.class));
+        verify(policyChain, times(1)).streamFailWith(resultCaptor.capture());
+        assertEquals(JSON_THREAT_MAX_NAME_LENGTH_KEY, resultCaptor.getValue().key());
     }
 
     @Test
@@ -131,7 +210,8 @@ public class JsonThreatProtectionPolicyTest {
         readWriteStream.write(Buffer.buffer("{ \"valid\": false, \"array\": [ 1, 2, 3 ], \"container\": { \"a\": \"123456789\" } }"));
         readWriteStream.end();
 
-        verify(policyChain, times(1)).streamFailWith(any(PolicyResult.class));
+        verify(policyChain, times(1)).streamFailWith(resultCaptor.capture());
+        assertEquals(JSON_THREAT_MAX_VALUE_LENGTH_KEY, resultCaptor.getValue().key());
     }
 
     @Test
@@ -145,7 +225,8 @@ public class JsonThreatProtectionPolicyTest {
         readWriteStream.write(Buffer.buffer("{ \"valid\": false, \"array\": [ 1, 2, 3 ], \"container\": { \"a\": \"123456789\" } }"));
         readWriteStream.end();
 
-        verify(policyChain, times(1)).streamFailWith(any(PolicyResult.class));
+        verify(policyChain, times(1)).streamFailWith(resultCaptor.capture());
+        assertEquals(JSON_THREAT_MAX_ENTRIES_KEY, resultCaptor.getValue().key());
     }
 
     @Test
@@ -159,7 +240,51 @@ public class JsonThreatProtectionPolicyTest {
         readWriteStream.write(Buffer.buffer("{ \"valid\": false, \"array\": [ 1, 2, 3 ], \"container\": { \"a\": \"123456789\" } }"));
         readWriteStream.end();
 
+        verify(policyChain, times(1)).streamFailWith(resultCaptor.capture());
+        assertEquals(JSON_MAX_ARRAY_SIZE_KEY, resultCaptor.getValue().key());
+    }
+
+    @Test
+    public void shouldRejectWhenMaxArrayOfObjectSizeExceeded() {
+
+        configuration.setMaxArraySize(2);
+        ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
+
+        assertNotNull(readWriteStream);
+
+        readWriteStream.write(Buffer.buffer(JSON));
+        readWriteStream.end();
+
         verify(policyChain, times(1)).streamFailWith(any(PolicyResult.class));
+    }
+
+    @Test
+    public void shouldRejectWhenMaxArraySizeExceededInSubArray() {
+
+        configuration.setMaxArraySize(4);
+        ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
+
+        assertNotNull(readWriteStream);
+
+        readWriteStream.write(Buffer.buffer("{\"a\":[\"1\",\"2\",\"3\",[\"1\",\"2\",\"3\",[\"1\",\"2\",\"3\",\"4\",\"5\"]]]}"));
+        readWriteStream.end();
+
+        verify(policyChain, times(1)).streamFailWith(resultCaptor.capture());
+        assertEquals(JSON_MAX_ARRAY_SIZE_KEY, resultCaptor.getValue().key());
+    }
+
+    @Test
+    public void shouldAcceptWhenMaxArraySizeDoesNotExceed() {
+
+        configuration.setMaxArraySize(4);
+        ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
+
+        assertNotNull(readWriteStream);
+
+        readWriteStream.write(Buffer.buffer("{\"a\":[\"1\",\"2\",\"3\",[\"1\",\"2\",\"3\",[\"1\",\"2\",\"3\",\"4\"]]]}"));
+        readWriteStream.end();
+
+        verifyNoInteractions(policyChain);
     }
 
     @Test
@@ -173,6 +298,36 @@ public class JsonThreatProtectionPolicyTest {
         readWriteStream.write(Buffer.buffer("{ \"valid\": false, \"array\": [ 1, 2, 3 ], \"container\": { \"a\": \"123456789\" } }"));
         readWriteStream.end();
 
-        verify(policyChain, times(1)).streamFailWith(any(PolicyResult.class));
+        verify(policyChain, times(1)).streamFailWith(resultCaptor.capture());
+        assertEquals(JSON_THREAT_MAX_DEPTH_KEY, resultCaptor.getValue().key());
+    }
+
+    @Test
+    public void shouldRejectWhenMaxDepthInArraysExceeded() {
+
+        configuration.setMaxDepth(6);
+        ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
+
+        assertNotNull(readWriteStream);
+
+        readWriteStream.write(Buffer.buffer("{ \"valid\": false, \"array\": [ 1, 2, 3 ], \"container\": { \"a\": [ [ [ [ [ \"123456789\" ] ] ] ] ] } }"));
+        readWriteStream.end();
+
+        verify(policyChain, times(1)).streamFailWith(resultCaptor.capture());
+        assertEquals(JSON_THREAT_MAX_DEPTH_KEY, resultCaptor.getValue().key());
+    }
+
+    @Test
+    public void shouldAcceptWhenMaxDepthInArraysDoesNotExceed() {
+
+        configuration.setMaxDepth(7);
+        ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
+
+        assertNotNull(readWriteStream);
+
+        readWriteStream.write(Buffer.buffer("{ \"valid\": false, \"array\": [ 1, 2, 3 ], \"container\": { \"a\": [ [ [ [ [ \"123456789\" ] ] ] ] ] } }"));
+        readWriteStream.end();
+
+        verifyNoInteractions(policyChain);
     }
 }
