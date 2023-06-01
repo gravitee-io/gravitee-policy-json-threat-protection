3171422fc3a4ac5687f5cdd22880fdb98e670198
Kamiel Ahmadpour <kamiel.ahmadpour@graviteesource.com>
2023-06-01 13:50:16 +0100

    feat: convert to EE plugin

--- src/main/java/com/graviteesource/policy/threatprotection/json/JsonThreatProtectionPolicy.java
+++ src/main/java/com/graviteesource/policy/threatprotection/json/JsonThreatProtectionPolicy.java
@@ -1,23 +1,9 @@
-/**
- * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package io.gravitee.policy.threatprotection.json;
+package com.graviteesource.policy.threatprotection.json;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
+import com.graviteesource.policy.threatprotection.json.deployer.JsonThreatProtectionPolicyDeploymentLifecycle;
 import io.gravitee.common.http.HttpHeaders;
 import io.gravitee.common.http.HttpStatusCode;
 import io.gravitee.common.http.MediaType;
@@ -26,6 +12,7 @@ import io.gravitee.gateway.api.buffer.Buffer;
 import io.gravitee.gateway.api.stream.BufferedReadWriteStream;
 import io.gravitee.gateway.api.stream.ReadWriteStream;
 import io.gravitee.gateway.api.stream.SimpleReadWriteStream;
+import io.gravitee.plugin.api.annotations.Plugin;
 import io.gravitee.policy.api.PolicyChain;
 import io.gravitee.policy.api.PolicyResult;
 import io.gravitee.policy.api.annotations.OnRequestContent;
@@ -36,6 +23,7 @@ import java.util.Collections;
  * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
  * @author GraviteeSource Team
  */
+@Plugin(deployment = JsonThreatProtectionPolicyDeploymentLifecycle.class)
 public class JsonThreatProtectionPolicy {
 
     public static final String BAD_REQUEST = "Bad Request";
